7c6e4f9e6bdf6c3f018d392c41eb26f73eb60e95
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2021-01-09 19:46:44 -0600

    add difficulty and hide all

---
+++ src/components/DifficultyOptions.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <button
+      :class="{ active: difficulty === 'easy' }"
+      @click="selectDifficulty('easy')"
+    >
+      Easy
+    </button>
+    <button
+      :class="{ active: difficulty === 'medium' }"
+      @click="selectDifficulty('medium')"
+    >
+      Medium
+    </button>
+    <button
+      :class="{ active: difficulty === 'hard' }"
+      @click="selectDifficulty('hard')"
+    >
+      Hard
+    </button>
+    <button
+      :class="{ active: difficulty === 'all' }"
+      @click="selectDifficulty('all')"
+    >
+      Show All
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["difficulty"],
+  methods: {
+    selectDifficulty(difficulty) {
+      this.$emit("difficulty-change", difficulty);
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  border: 5px solid black;
+  color: goldenrod;
+  padding: 15px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  margin: 0 2px;
+  font-weight: bold;
+}
+
+button.active {
+  background-color: goldenrod;
+  color: black;
+}
+</style>
--- src/components/TriviaGame.vue
+++ src/components/TriviaGame.vue
@@ -1,27 +1,54 @@
 <template>
-  <div class="questions">
-    <div v-for="card in trivia" :key="card.id">
-      <flash-card :card="card" @toggle="handleToggle" />
+  <div>
+    <h1>Star Wars Trivia</h1>
+    <button @click="hideAll" class="flat-button">HIDE ALL</button>
+    <div class="difficulty-options">
+      <difficulty-options
+        :difficulty="selectedDifficulty"
+        @difficulty-change="handleDifficultyChange"
+      />
+    </div>
+    <div class="questions">
+      <div v-for="card in displayedTrivia" :key="card.id">
+        <flash-card :card="card" @toggle="handleToggle" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import trivia from "../trivia";
+import DifficultyOptions from "./DifficultyOptions.vue";
 import FlashCard from "./FlashCard";
 
 export default {
-  components: { FlashCard },
+  components: { FlashCard, DifficultyOptions },
 
   data() {
     return {
-      trivia: [...trivia]
+      trivia: [...trivia],
+      selectedDifficulty: "all"
     };
   },
 
   methods: {
     handleToggle(card) {
       card.answerShown = !card.answerShown;
+    },
+    handleDifficultyChange(difficulty) {
+      this.selectedDifficulty = difficulty;
+    },
+    hideAll() {
+      this.trivia.forEach(t => (t.answerShown = false));
+    }
+  },
+
+  computed: {
+    displayedTrivia() {
+      if (this.selectedDifficulty === "all") {
+        return this.trivia;
+      }
+      return this.trivia.filter(t => t.difficulty === this.selectedDifficulty);
     }
   }
 };
@@ -33,5 +60,14 @@ export default {
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-around;
+}
+
+.flat-button {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  margin-bottom: 10px;
+  font-size: 20px;
+  color: goldenrod;
 }
 </style>
