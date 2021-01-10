07a5dfeb6e30055a8a540ade13a830c1984decd6
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2021-01-09 18:32:12 -0600

    save trivia questions in component data

---
+++ src/components/TriviaGame.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import trivia from "../trivia";
+
+export default {
+  data() {
+    return {
+      trivia: [...trivia]
+    };
+  }
+};
+</script>
+
+<style></style>
